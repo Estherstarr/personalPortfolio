46abf9685bed347444b6ed5837d303e6908a18a5
Estherstarr <60798978+Estherstarr@users.noreply.github.com>
2022-03-10 20:50:01 +0100

    Update the project section in the index.html file  using js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
 }));
 
 // array object to hold projects data
-const data = [
+const data = (
   {
     id: 'project-1',
     name: 'Tonic',
@@ -22,13 +22,15 @@ const data = [
       year: '2015',
     },
     description: {
-      summary: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      summary:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s',
     },
     image: {
       mobile: 'assets/images/mobileviewimages/SnapshootPortfolio.svg',
-      desktop: 'assets/images/desktopviewimages/PortoflioCard/SnapshootPortfolio.svg',
+      desktop:
+        'assets/images/desktopviewimages/PortoflioCard/SnapshootPortfolio.svg',
     },
     technologies: {
       html: 'html',
@@ -46,9 +48,10 @@ const data = [
       year: '2015',
     },
     description: {
-      summary: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      summary:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
       portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s',
     },
     image: {
       mobile: 'assets/images/mobileviewimages/Snapshootm2.svg',
@@ -71,9 +74,10 @@ const data = [
       year: '2015',
     },
     description: {
-      summary: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      summary:
+        'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
       portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s',
     },
     image: {
       mobile: 'assets/images/mobileviewimages/Snapshoot3.svg',
@@ -96,9 +100,10 @@ const data = [
       year: '2008',
     },
     description: {
-      summary: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+      summary:
+        'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
       portfolio:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s",
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s',
     },
     image: {
       mobile: 'assets/images/mobileviewimages/Snapshoot4.svg',
@@ -111,5 +116,43 @@ const data = [
       javascript: 'javascript',
     },
     section: 'section-img-right',
-  },
-];
+  });
+
+// pop up section
+
+const workPortfolio = document.querySelector('#work-portfolio');
+const portfolioModal = document.querySelector('#portfolio-modal');
+
+workPortfolio.classList.add('projects');
+
+Array.from(workPortfolio.children).forEach((child, index) => {
+  child.innerHTML = `
+    <section class="${data[index].section}">
+      <ul>
+        <li><img src="${data[index].image.mobile}" alt="icon" class="img-1-1" /></li>
+        <li><img src="${data[index].image.desktop}" alt="icon" class="img-1-2" /></li>
+      </ul>
+      <div class="block-1">
+        <div class="text-group">
+          <h2 class="lg-text-2">${data[index].name}</h2>
+          <div class="client-info">
+            <h3 class="client">${data[index].job.client}</h3>
+            <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
+            <h3 class="role">${data[index].job.role}</h3>
+            <a href="#"><img src="assets/Icons/Counter.svg" alt="counter" /></a>
+            <h3 class="year-label">${data[index].job.year}</h3>
+          </div>
+        </div>
+        <h3 class="sm-text-2">${data[index].description.summary}</h3>
+        <ul class="tags">
+          <li>${data[index].technologies.html}</li>
+          <li>${data[index].technologies.css}</li>
+          <li>${data[index].technologies.javascript}</li>
+        </ul>
+  
+        <div class="actions" id="${data[index].id}">
+          <button class="btn" id="btn" type="button">See Project</button>
+        </div>
+      </div>
+    </section>`;
+});
